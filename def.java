4a07bf0ff765cb0201de622b3d7ffc12397e56b2
priyanka2518 <124593675+priyanka2518@users.noreply.github.com>
2023-08-29 19:58:25 +0530

    Add files via upload

---
+++ def.java
@@ -0,0 +1,16 @@
+//check the default values for all the data types
+class  def
+{
+	public static void main(String[] args) 
+	{
+		int a[];
+		System.out.println(a[]);
+		/*float[] b;
+		char[] c;
+		long[] d;
+		double[] e;
+		short[] f;
+		byte[] g;
+		String[] h;*/
+	}
+}
